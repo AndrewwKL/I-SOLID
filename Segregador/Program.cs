7c0c2d1ab59ba2fea5b5f24774ae74a79a75301b
AndrewwKL <142675561+AndrewwKL@users.noreply.github.com>
2023-10-17 21:33:58 -0500

    Update Program.cs

--- Segregador/Program.cs
+++ Segregador/Program.cs
@@ -4,6 +4,9 @@
     {
         static void Main(string[] args)
         {
+            //614222710 Oscar Daniel Arcos Realpe
+            //506222714 Brayan Stiben Hernandez Fonseca
+            //506222067    Andres Ricardo Caceres Ortiz
             Console.WriteLine("Bienvenido!");
             Console.WriteLine("Seleccione la función");
             int num=int.Parse(Console.ReadLine());
